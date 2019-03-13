44d9ef17c954d731728a49e3c74b8d6271ef01fa
Felipe <fborja@g.hmc.edu>
2019-03-13 02:52:03 -0700

    Finished up steering and fixed problems with launch file

--- nodes/robot_steering.py
+++ nodes/robot_steering.py
@@ -11,23 +11,32 @@ class robot_steering():
 
     def __init__(self):
         # Initialize node, publisher, and subscriber
-        rospy.init_node('robot_steering', anonymous = True)
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
+        rospy.init_node('robot_steering', anonymous = True)
         rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.robot_steering_callback)
-    
-    
-    def robot_steering_callback(self, fid_trans):
-        if(fid_trans.transforms):
-            trans = fid_trans.transforms.transform.translation
-            rot = fid_trans.transforms.transform.rotation
-            
+        
+        rate = rospy.Rate(10) # 10 Hz
+
+        self.vel = Twist() 
+        self.vel.linear = Vector3(0,0,0)
+        self.vel.angular = Vector3(0,0,0)
+
+        while not rospy.is_shutdown():
+            self.pub.publish(self.vel)
+            rate.sleep()
 
         
+    def robot_steering_callback(self, fid_trans):
+        if len(fid_trans.transforms) > 0:
+            fid_linear = fid_trans.transforms[0].transform.translation.z
+            fid_rot = fid_trans.transforms[0].transform.rotation
+            rot = np.array([fid_rot.x, fid_rot.y, fid_rot.z, fid_rot.w])
 
-    
+            rot_Euler = tf.transformations.euler_from_quaternion(rot)[2]
 
-
-
+            self.vel.linear.x = 0.5 * fid_linear 
+            self.vel.angular.z = 1.0 * rot_Euler 
+            
 
 if __name__ == '__main__':
     try:
