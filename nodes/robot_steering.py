0e9c6d20896e55e4c3945381b9a1bb2b49214d34
Felipe <fborja@g.hmc.edu>
2019-04-29 15:00:48 -0700

    Improved robot steering.

--- nodes/robot_steering.py
+++ nodes/robot_steering.py
@@ -17,6 +17,8 @@ class robot_steering():
         
         rate = rospy.Rate(10) # 10 Hz
 
+        self.inactive_timer = 0
+
         self.vel = Twist() 
         self.vel.linear = Vector3(0,0,0)
         self.vel.angular = Vector3(0,0,0)
@@ -28,14 +30,28 @@ class robot_steering():
         
     def robot_steering_callback(self, fid_trans):
         if len(fid_trans.transforms) > 0:
+            self.inactive_timer = 0
+
             fid_linear = fid_trans.transforms[0].transform.translation.z
             fid_rot = fid_trans.transforms[0].transform.rotation
             rot = np.array([fid_rot.x, fid_rot.y, fid_rot.z, fid_rot.w])
 
             rot_Euler = tf.transformations.euler_from_quaternion(rot)[2]
 
-            self.vel.linear.x = 0.5 * fid_linear 
-            self.vel.angular.z = 1.0 * rot_Euler 
+            # This is a simple linear velocity command
+            self.vel.linear.x = 0.5 * fid_linear + 0.2
+
+            # This linear velocity command makes:
+            # closer tag -> faster speed, farther tag -> slower speed
+            #self.vel.linear.x = 2.10 - 2.5 * fid_linear
+            print(fid_linear)
+            self.vel.angular.z = 1.2 * rot_Euler
+        else:
+            self.inactive_timer += 1
+            if self.inactive_timer > 5:
+                # If no fiducial transforms are detected after 1 sec, stop.
+                self.vel.linear.x = 0
+                self.vel.angular.z = 0
             
 
 if __name__ == '__main__':
